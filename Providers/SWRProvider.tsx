439bc94aae61ed66c6d1d218fe7117e118fc0ba7
rashidpkn <muhammed916rashid@gmail.com>
2025-10-08 16:56:51 +0530

    SWR Profileder

---
+++ Providers/SWRProvider.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import api from '@/lib/axios';
+import React from 'react'
+import { SWRConfig } from 'swr';
+
+export default function SWRProvider({children}:{children:React.ReactNode}) {
+  return (
+     <SWRConfig
+            value={{
+              fetcher: async (url: string) => {
+                const res = await api.get(url);
+                return res.data;
+              },
+              shouldRetryOnError: true,
+              errorRetryCount: 3,
+              errorRetryInterval: 3000,
+              revalidateOnFocus: true,
+              revalidateOnReconnect: true,
+              revalidateIfStale: false,
+              refreshInterval: 0, // set >0 if you need periodic refresh
+              suspense: false,
+              onError: (err) => {
+                console.error("SWR error:", err.message);
+              },
+            }}
+          >
+                {children}
+            </SWRConfig>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import { AuthProvider } from "@/auth/context/auth-context";
+import SWRProvider from "@/Providers/SWRProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +31,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AuthProvider>{children}</AuthProvider>
+        <AuthProvider>
+         <SWRProvider>
+            {children}
+         </SWRProvider>
+          
+        </AuthProvider>
         <Toaster />
       </body>
     </html>
