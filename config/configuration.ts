6fb3ee3083ce85303b8e0f2020b02d5aa82a1fdc
rashidpkn <muhammed916rashid@gmail.com>
2025-12-01 15:38:48 +0530

    fix: Add type assertion for `in_house_lab_id` environment variable.

--- config/configuration.ts
+++ config/configuration.ts
@@ -1,4 +1,4 @@
 export default () => ({
   backendUrl: process.env.NEXT_PUBLIC_BACKEND_URL,
-  in_house_lab_id: process.env.NEXT_PUBLIC_IN_HOUSE_LAB_ID
+  in_house_lab_id: process.env.NEXT_PUBLIC_IN_HOUSE_LAB_ID as string,
 });
