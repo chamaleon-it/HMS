b44355f74b9ede207feb8d55213fdc4dbd5e71ef
rashidpkn <muhammed916rashid@gmail.com>
2026-01-05 15:10:00 +0530

    feat: Implement new hospital branding, logo, and contact details in print components, with image loading optimizations.

--- components/print/HospitalName.tsx
+++ components/print/HospitalName.tsx
@@ -1,18 +1,21 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function HospitalName() {
+
     return (
         <div className="flex gap-5 items-center">
-            <div className="h-14 w-14 bg-slate-900 flex items-center justify-center text-white text-2xl font-black">
-                S
+            <div className="h-14 w-14 flex items-center justify-center text-white text-2xl font-black relative">
+                <Image src={"/print/logo.png"} alt="Logo" width={56} height={56} className='w-full h-full grayscale' loading='eager' fetchPriority='high' priority />
+
             </div>
             <div>
-                <h1 className="text-xl font-black text-slate-900 tracking-tight leading-none mb-1">SYNAPSE HOSPITAL</h1>
+                <h1 className="text-xl font-black text-slate-900 tracking-tight leading-none mb-1">Mark Hospital</h1>
                 <p className="text-[10px] font-bold text-slate-700 uppercase">Multi-Speciality Care & Research Institute</p>
                 <div className="mt-1 text-[10px] text-slate-600 leading-tight font-medium">
-                    <p>123 Medical Enclave, Health City, Bangalore - 560001</p>
-                    <p className="font-bold text-slate-800">GSTIN: 29AAAAA0000A1Z5 | DL No: KA-BNG-123456 | <span className="text-black">State Code: 29</span></p>
-                    <p>Tel: +91 80 4455 6677 | Email: info@synapsehms.com</p>
+                    <p>Pothukallu P.O, Nilambur, Malappuram, India - 679334</p>
+                    <p className="font-bold text-slate-800">DIGIPIN: MC9-955-6T2F | <span className="text-black">State Code: 29</span></p>
+                    <p>Tel: +91 83019 26155, 04931 240077 | Email: hospitalmark@gmail.com</p>
                 </div>
             </div>
         </div>
--- components/print/Watermark.tsx
+++ components/print/Watermark.tsx
@@ -4,8 +4,8 @@ const logo = "/print/logo.png";
 
 export default function Watermark() {
     return (
-        <div className="fixed inset-0 flex items-center justify-center pointer-events-none opacity-[0.1] hidden print:flex grayscale" style={{ zIndex: 100 }}>
-            <img src={logo} alt="watermark" className="w-[12cm]" />
+        <div className="fixed inset-0  items-center justify-center pointer-events-none opacity-[0.1] hidden print:flex grayscale" style={{ zIndex: 100 }}>
+            <img src={logo} alt="watermark" className="w-[12cm]" loading='eager' fetchPriority='high' />
         </div>
     )
 }
