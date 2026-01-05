0844813f2f9e2b80666f3a2cd73a240300d2db88
rashidpkn <muhammed916rashid@gmail.com>
2026-01-05 13:00:40 +0530

    refactor: remove redundant z-50 class from Watermark component

--- components/print/Watermark.tsx
+++ components/print/Watermark.tsx
@@ -4,7 +4,7 @@ const logo = "/print/logo.png";
 
 export default function Watermark() {
     return (
-        <div className="fixed inset-0 flex z-50 items-center justify-center pointer-events-none opacity-[0.1] hidden print:flex grayscale" style={{ zIndex: 100 }}>
+        <div className="fixed inset-0 flex items-center justify-center pointer-events-none opacity-[0.1] hidden print:flex grayscale" style={{ zIndex: 100 }}>
             <img src={logo} alt="watermark" className="w-[12cm]" />
         </div>
     )
