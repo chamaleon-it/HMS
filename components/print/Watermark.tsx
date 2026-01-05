bee9389fbc1af18103fd37ddc7f1c45697e6856c
rashidpkn <muhammed916rashid@gmail.com>
2026-01-05 11:37:49 +0530

    feat: Add watermark component and apply it to pharmacy print receipts and prescriptions.

---
+++ components/print/Watermark.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const logo = "/print/logo.png";
+
+export default function Watermark() {
+    return (
+        <div className="fixed inset-0 flex items-center justify-center pointer-events-none opacity-[0.1] hidden print:flex grayscale" style={{ zIndex: 100 }}>
+            <img src={logo} alt="watermark" className="w-[12cm]" />
+        </div>
+    )
+}
