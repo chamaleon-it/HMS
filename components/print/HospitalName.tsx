9a724bab0d543cc6dc9c9b8f4b3e881a55544674
rashidpkn <muhammed916rashid@gmail.com>
2026-01-06 09:56:11 +0530

    feat: update hospital logo and refine print header layout and styling.

--- components/print/HospitalName.tsx
+++ components/print/HospitalName.tsx
@@ -4,16 +4,16 @@ import React from 'react'
 export default function HospitalName() {
 
     return (
-        <div className="flex gap-5 items-center">
-            <div className="size-[100px] flex items-center justify-center text-white text-2xl font-black relative">
-                <Image src={"/print/logo-white.png"} alt="Logo" width={100} height={100} className='w-full h-full' loading='eager' fetchPriority='high' priority />
-
+        <div className="flex gap-1.5 items-center">
+            <div className="flex items-center justify-center mt-2">
+                <Image src={"/print/logo-white.png"} alt="Logo" width={90} height={90} className='w-full h-full' loading='eager' fetchPriority='high' priority />
             </div>
-            <div>
+            <div className="flex flex-col justify-center">
                 <h1 className="text-2xl font-bold tracking-tight">Mark Hospital</h1>
-                <p className="text-xs opacity-75 mt-1">Pothukallu P.O, Nilambur, Malappuram, India - 679334</p>
-                <p className="text-xs opacity-75 mt-1">DIGIPIN: MC9-955-6T2F</p>
-                <p className="text-xs opacity-75 mt-1">Tel: +91 83019 26155, 04931 240077 , Email: hospitalmark@gmail.com</p>
+                <p className="text-xs opacity-75">Pothukallu P.O, Nilambur, Malappuram, India - 679334</p>
+                <p className="text-xs opacity-75">DIGIPIN: MC9-955-6T2F</p>
+                <p className="text-xs opacity-75">Tel: +91 83019 26155, 04931 240077</p>
+                <p className="text-xs opacity-75">Email: hospitalmark@gmail.com</p>
             </div>
         </div>
     )
