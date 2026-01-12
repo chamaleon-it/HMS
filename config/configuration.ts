a405c0fa25d358f07889b482368c8809f942e1c3
rashidpkn <muhammed916rashid@gmail.com>
2026-01-12 11:24:26 +0530

    .env

--- config/configuration.ts
+++ config/configuration.ts
@@ -1,4 +1,4 @@
 export default () => ({
-  backendUrl: process.env.NEXT_PUBLIC_BACKEND_URL,
-  in_house_lab_id: process.env.NEXT_PUBLIC_IN_HOUSE_LAB_ID as string,
+  backendUrl: "http://localhost:3001", //process.env.NEXT_PUBLIC_BACKEND_URL,
+  in_house_lab_id: "6916a4bdbab30920d8f56494" //process.env.NEXT_PUBLIC_IN_HOUSE_LAB_ID as string,
 });
