b6600fd42cda42f609225d654095aee547abceab
rashidpkn <muhammed916rashid@gmail.com>
2026-03-10 13:00:04 +0530

    fix: correct hospital address in configuration.

--- config/configuration.ts
+++ config/configuration.ts
@@ -2,7 +2,7 @@ export default () => ({
   backendUrl: process.env.NEXT_PUBLIC_BACKEND_URL,
   in_house_lab_id: process.env.NEXT_PUBLIC_IN_HOUSE_LAB_ID as string,
   hospitalName: "Mukalel Poly Clinic & Diagnostic Centre" as string,
-  hospitalAddress: "Chermabadi, Erumad, Tamil Nadu 643239" as string,
+  hospitalAddress: "Erumad, Tamil Nadu 643239" as string,
   hospitalPhone: "+91 82484 99860" as string,
   hospitalEmail: ""
 });
