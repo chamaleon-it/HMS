7ab978682d30f28ae6922c1b872b9a8d2be087d7
rashidpkn <muhammed916rashid@gmail.com>
2026-03-10 14:17:44 +0530

    style: Adjust hospital logo image dimensions in the print component.

--- components/print/HospitalName.tsx
+++ components/print/HospitalName.tsx
@@ -6,7 +6,7 @@ export default function HospitalName() {
     return (
         <div className="flex gap-3 items-center">
             <div className="h-full flex justify-center items-center">
-                <img src={"/print/logo.png"} alt="Logo" width={100} height={80} className='w-[75px] h-[60px]' />
+                <img src={"/print/logo.png"} alt="Logo" width={60} className='w-[60px]' />
             </div>
             <div className="flex flex-col gap-0.5">
                 <h1 className="text-xl font-bold tracking-tight">{configuration().hospitalName}</h1>
