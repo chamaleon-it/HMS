717f7f7d6d8b17f69cc4e597589b4a735b8abf82
Muhammed Rashid N <muhammed916rashid@gmail.com>
2026-06-01 12:49:35 +0530

    feat: enable strip count column and input field in bulk update table

--- app/dashboard/pharmacy/purchase-entry/BulkUpdateTable.tsx
+++ app/dashboard/pharmacy/purchase-entry/BulkUpdateTable.tsx
@@ -451,7 +451,7 @@ export default function BulkUpdateTable({ items, lowStockThreshold, onSave }: Pr
                                 <TableHead className="min-w-[150px] text-[11px] font-semibold uppercase text-slate-200 py-4 tracking-wider">PRODUCT NAME</TableHead>
                                 <TableHead className="text-[11px] font-semibold uppercase text-slate-200 py-4 tracking-wider">BATCH</TableHead>
                                 <TableHead className="text-[11px] font-semibold uppercase text-slate-200 py-4 text-center tracking-wider">PACK</TableHead>
-                                {/* <TableHead className="text-[11px] font-semibold uppercase text-slate-200 py-4 text-center tracking-wider">STRIP COUNT</TableHead> */}
+                                <TableHead className="text-[11px] font-semibold uppercase text-slate-200 py-4 text-center tracking-wider">STRIP COUNT</TableHead>
                                 <TableHead className="text-[11px] font-semibold uppercase text-slate-200 py-4 text-center tracking-wider min-w-[85px]">QTY</TableHead>
                                 <TableHead className="text-[11px] font-semibold uppercase text-slate-200 py-4 text-center tracking-wider min-w-[85px]">MRP</TableHead>
                                 <TableHead className="text-[11px] font-semibold uppercase text-slate-200 py-4 tracking-wider">EXPIRY</TableHead>
@@ -518,7 +518,7 @@ export default function BulkUpdateTable({ items, lowStockThreshold, onSave }: Pr
                                                 onKeyDown={(e) => handleKeyDown(e, item.id, "pack")}
                                             />
                                         </TableCell>
-                                        {/* <TableCell className="p-2">
+                                        <TableCell className="p-2">
                                             <Input
                                                 type="number"
                                                 data-field="noOfPack"
@@ -527,7 +527,7 @@ export default function BulkUpdateTable({ items, lowStockThreshold, onSave }: Pr
                                                 onChange={(e) => updateNewItem(item.id, "noOfPack", Number(e.target.value))}
                                                 onKeyDown={(e) => handleKeyDown(e, item.id, "noOfPack")}
                                             />
-                                        </TableCell> */}
+                                        </TableCell>
                                         <TableCell className="p-2">
                                             <Input
                                                 type="number"
