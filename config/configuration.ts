cacdfe8b468b5712e545937e503fa05c37349798
Muhammed Rashid N <muhammed916rashid@gmail.com>
2026-06-24 16:06:13 +0530

    refactor: remove in_house_lab_id configuration from environment variables and frontend config

--- config/configuration.ts
+++ config/configuration.ts
@@ -1,6 +1,5 @@
 export default () => ({
   backendUrl: process.env.NEXT_PUBLIC_BACKEND_URL,
-  in_house_lab_id: process.env.NEXT_PUBLIC_IN_HOUSE_LAB_ID as string,
   hospitalName: process.env.NEXT_PUBLIC_HOSPITAL_NAME,
   hospitalAddress: process.env.NEXT_PUBLIC_HOSPITAL_ADDRESS,
   hospitalPhone: process.env.NEXT_PUBLIC_HOSPITAL_PHONE,
@@ -9,9 +8,3 @@ export default () => ({
   logo: process.env.NEXT_PUBLIC_LOGO || "/logo.png",
   gstIn: process.env.NEXT_PUBLIC_GSTIN
 });
-
-
-// export default () => ({
-//   backendUrl: "http://localhost:3001",
-//   in_house_lab_id: "68e4e8c7534364541c8deeb2"
-// });
