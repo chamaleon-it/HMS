db48f06982b7586162b3162c2668f58bfc80ef64
Muhammed Rashid N <muhammed916rashid@gmail.com>
2026-07-20 10:40:54 +0530

    style: increase calendar layout dimensions and spacing for improved readability in DateTimePicker

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -46,7 +46,7 @@ function Calendar({
           "flex gap-4 flex-col md:flex-row relative",
           defaultClassNames.months
         ),
-        month: cn("flex flex-col w-full gap-2", defaultClassNames.month),
+        month: cn("flex flex-col w-full gap-3", defaultClassNames.month),
         nav: cn(
           "flex items-center gap-1 w-full absolute top-0 inset-x-0 justify-between",
           defaultClassNames.nav
@@ -90,7 +90,7 @@ function Calendar({
           "text-muted-foreground rounded-md flex-1 font-normal text-[0.8rem] select-none",
           defaultClassNames.weekday
         ),
-        week: cn("flex w-full mt-1", defaultClassNames.week),
+        week: cn("flex w-full mt-1.5", defaultClassNames.week),
         week_number_header: cn(
           "select-none w-(--cell-size)",
           defaultClassNames.week_number_header
